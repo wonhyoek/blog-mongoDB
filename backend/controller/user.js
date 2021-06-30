feec0b6d217492717c4740a15e994fa82d436048
wonhyoek <epahsejtmxm@naver.com>
2021-06-30 16:02:21 +0900

    set express mongoose basic configuration

---
+++ backend/controller/user.js
@@ -0,0 +1,3 @@
+exports.getUsers = async (req, res, next) => {
+    res.send('user router')
+}
---
+++ backend/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require('dotenv');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const cors = require('cors');
+const userRouter = require('./router/user');
+
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(
+    process.env.MONGO_URL, 
+    {useNewUrlParser: true, useUnifiedTopology: true}
+).then(() => console.log('Mongoose Connected...'))
+.catch(err => console.log(err));
+
+
+
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(morgan('dev'));
+
+
+app.use('/api/users', userRouter);
+
+
+
+app.listen(PORT, () => 
+    console.log(`Server is running on http://localhost:${PORT}`));
